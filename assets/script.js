9881e7ec8e2b65d52e45c884268a6d92a427383d
tooqk4u <tooqk4u@protonmail.com>
2021-12-19 22:53:40 -0500

    finished CSS,  added comments to Javascript and minor HTML changes

--- assets/script.js
+++ assets/script.js
@@ -15,127 +15,119 @@ let displayMessage = document.querySelector(".save-message")
 let timeInterval;
 let correctAnswers = 0;
 let scoreArray = []
+if(localStorage.getItem("playerInfo")){
+  scoreArray = JSON.parse(localStorage.getItem("playerInfo"))
+}
 let questions = [
   {
     question: "1. What is the HTML tag under which one can write the JavaScript code?",
 
-    choice1: "1. javascript",
-    choice2: "2. scripted",
-    choice3: "3. js",
-    choice4: "4. script",
-    answer: "4. script",
+    choice1: "a. javascript",
+    choice2: "b. scripted",
+    choice3: "c. js",
+    choice4: "d. script",
+    answer: "d. script",
   },
   {
     question: "2. Commonly used data types do NOT include:",
 
-    choice1: "1. strings",
-    choice2: "2. boxes",
-    choice3: "3. booleans",
-    choice4: "4. character",
-    answer: "2. boxes",
+    choice1: "a. strings",
+    choice2: "b. boxes",
+    choice3: "c. booleans",
+    choice4: "d. character",
+    answer: "b. boxes",
   },
   {
     question: "3. What does the word DOM stand for in JavaScript?",
 
-    choice1: "1. Document Object Model",
-    choice2: "2. Document Outline Model",
-    choice3: "3. Domain Outline Model",
-    choice4: "4. Domain Object Model",
-    answer: "1. Document Object Model",
+    choice1: "a. Document Object Model",
+    choice2: "b. Document Outline Model",
+    choice3: "c. Domain Outline Model",
+    choice4: "d. Domain Object Model",
+    answer: "a. Document Object Model",
   },
   {
     question: "4. Global variables should be defined in which section of the JavaScript file?",
 
-    choice1: "1. Bottom",
-    choice2: "2. Middle",
-    choice3: "3. Beginning",
-    choice4: "4. Anywhere",
-    answer: "3. Beginning",
+    choice1: "a. Bottom",
+    choice2: "b. Middle",
+    choice3: "c. Beginning",
+    choice4: "d. Anywhere",
+    answer: "c. Beginning",
   },
   {
     question: "5. What are variables used for in JavaScript?",
 
-    choice1: "1. Storing numbers, dates, or other values",
-    choice2: "2. Causing high-school algebra flashbacks",
-    choice3: "3. None of these answers",
-    choice4: "4. varying randomly",
-    answer: "1. Storing numbers, dates, or other values",
+    choice1: "a. Storing numbers, dates, or other values",
+    choice2: "b. Causing high-school algebra flashbacks",
+    choice3: "c. None of these answers",
+    choice4: "d. varying randomly",
+    answer: "a. Storing numbers, dates, or other values",
   },
   { 
     question: "6. What is the correct way to write a JavaScript array?",
 
-    choice1: "1. let colors = 'red', 'green', 'blue'",
-    choice2: "2. let colors = (1:'red', 2:'green', 3:'blue')",
-    choice3: "3. let colors = 1 = ('red'), 2 = ('green'), 3 = ('blue'",
-    choice4: "4. let colors = ['red', 'green', 'blue']",
-    answer: "4. let colors = ['red', 'green', 'blue']",
+    choice1: "a. let colors = 'red', 'green', 'blue'",
+    choice2: "b. let colors = (1:'red', 2:'green', 3:'blue')",
+    choice3: "c. let colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')",
+    choice4: "d. let colors = ['red', 'green', 'blue']",
+    answer: "d. let colors = ['red', 'green', 'blue']",
   },
 ];
-
-//time length of quiz
 let timeLeft = questions.length * 15;
 
-//Start button function
 startBtn.addEventListener("click", startQuiz);
-
-//Start quiz function
 function startQuiz() {
   displayQuestions.classList.remove("hide");
   introBody.classList.add("hide");
-  //Start the clock-set interval//
+  //start the clock-set interval//
   timeInterval = setInterval(countdown, 1000);
 
   showQuestion();
 }
-//Show questions function 
+
 function showQuestion() {
-  displayQuestions.innerHTML = `<h3> ${questions
-[questionsIndex].question} </h3>
-    <p><button class="choices btn">${questions
-[questionsIndex].choice1}</button></p>
-    <p><button class="choices btn">${questions
-[questionsIndex].choice2}</button></p>
-    <p><button class="choices btn">${questions
-[questionsIndex].choice3}</button></p>
-    <p><button class="choices btn">${questions
-[questionsIndex].choice4}</button></p>
+  displayQuestions.innerHTML = `<h3> ${questions[questionsIndex].question} </h3>
+    <p><button class="choices btn">${questions[questionsIndex].choice1}</button></p>
+    <p><button class="choices btn">${questions[questionsIndex].choice2}</button></p>
+    <p><button class="choices btn">${questions[questionsIndex].choice3}</button></p>
+    <p><button class="choices btn">${questions[questionsIndex].choice4}</button></p>
     <p> <h4 class="message"></h4></p>
     `;
+
+  let choices = document.querySelectorAll(".choices");
+  let message = document.querySelector(".message");
   
-    let choices = document.querySelectorAll(".choices");
-    let message = document.querySelector(".message");
 
-    // For loop to loop thru questions and answers
-    for (let i = 0; i < choices.length; i++) {
-      choices[i].addEventListener("click", function () {
-        let buttontext = this.textContent;
-        if (questionsIndex < questions.length) {
-          if (questions[questionsIndex].answer === buttontext) {
-            message.textContent = "Correct!";
-            correctAnswers++
-            console.log(correctAnswers)
-          } else {
-            message.textContent = "Wrong!" //The correct answer is " + questions[questionsIndex].answer;//
-            timeLeft = timeLeft - 15;
-          }
-          questionsIndex++;
-          if (questionsIndex < questions.length) {
-            setTimeout(showQuestion, 500);
-              
-         } else {
-          clearInterval(timeInterval);
-          displayQuestions.classList.add("hide");
-          initials.classList.remove("hide");
-         }
-         
+  for (let i = 0; i < choices.length; i++) {
+    choices[i].addEventListener("click", function () {
+      let buttontext = this.textContent;
+      if (questionsIndex < questions.length) {
+        if (questions[questionsIndex].answer === buttontext) {
+          message.textContent = "Correct!";
+          //correctAnswers++
+          console.log(correctAnswers)
+        } else {
+          message.textContent = "Wrong!" //The correct answer is " + questions[questionsIndex].ans
+          timeLeft = timeLeft - 15;
         }
-      });
-      console.log(correctAnswers)
-    }
-    correctAnswers/questions.length * 100 
-};
+        questionsIndex++;
+        if (questionsIndex < questions.length) {
+          setTimeout(showQuestion, 500);
+            
+       } else {
+        clearInterval(timeInterval);
+        displayQuestions.classList.add("hide");
+        initials.classList.remove("hide");
+        //showSaveBtn()
+       }
+      }
+    });
+    console.log(correctAnswers)
+  }  
+  //correctAnswers/questions.length * 100
+}
 
-//Timer function
 function countdown() {
   if (timeLeft >= 0) {
     timerE1.textContent = timeLeft;
@@ -149,13 +141,14 @@ function countdown() {
   }
 }
 
-//
 saveBtn.addEventListener("click", saveScores)
 function saveScores(){
-  let initialsInput = document.querySelector("#name").value
-  console.log(initialsInput)
+  let initialsInput = document.querySelector("#name").value;
+  
+  
+  console.log(initialsInput);
  
-  let score = timeLeft//correctAnswers/questions.length * 100;
+  //let score = timeLeft//correctAnswers/questions.length * 100;
   console.log(timeLeft);
 
   playerInfo = {
@@ -168,6 +161,7 @@ function saveScores(){
   highScore.classList.remove("hide"); 
   showHighScore() 
 }
+
 function showHighScore() {
   let tdstring = ""
   
@@ -184,6 +178,7 @@ function showHighScore() {
 
 score1.addEventListener("click", function(){
   introBody.classList.add("hide");
+  displayQuestions.classList.add("hide");
   initials.classList.add("hide");
   highScore.classList.remove("hide"); 
   showHighScore() 
